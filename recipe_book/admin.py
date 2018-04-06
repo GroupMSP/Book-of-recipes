14196737ef28873a705fd2ece1700f6e0b0d16e3
Vadim Tokarev <git-main@ya.ru>
2018-04-06 16:59:22 +0300

    Added IngredientType in admin.py

--- recipe_book/admin.py
+++ recipe_book/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register([RecipeType, Instruction, Recipe, Ingredient, Recipe_ingredient])
+admin.site.register([IngredientType, RecipeType, Instruction, Recipe, Ingredient, Recipe_ingredient])
