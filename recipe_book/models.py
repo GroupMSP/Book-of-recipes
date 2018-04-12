8453a09770616cfb6c791f59b52a4dc1b213914e
Vadim Tokarev <git-main@ya.ru>
2018-04-12 20:13:40 +0300

    set FileField not required in forms, add ingredients field in Recipe

--- recipe_book/models.py
+++ recipe_book/models.py
@@ -19,13 +19,17 @@ class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1023, blank=True)
     preparation_time = models.DurationField()
-    image = models.FileField(upload_to=FilePathGenerator(), null=True)
+    image = models.FileField(upload_to=FilePathGenerator(), null=True,
+        blank=True)
     id_type = models.ForeignKey(to=RecipeType, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField('Ingredient',
+        through='Recipe_ingredient', related_name='recipes')
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=63)
-    image = models.FileField(upload_to=FilePathGenerator(), null=True)
+    image = models.FileField(upload_to=FilePathGenerator(), null=True,
+        blank=True)
     id_type = models.ForeignKey(to=IngredientType, on_delete=models.CASCADE)
 
 
