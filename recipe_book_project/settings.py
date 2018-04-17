42da07f2dd2ea752a39d6a8354151c9295ad9770
Vadim Tokarev <git-main@ya.ru>
2018-04-18 02:33:27 +0300

    Set allow every host

--- recipe_book_project/settings.py
+++ recipe_book_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=*$m-0ytd=ng8ht%g&g@xmso7pk@zhi^%7jedqy2x#(653i6v0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
